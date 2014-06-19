74ebf7e06d514aa3203a63183bcbddc900b8a22e
Cameron Taggart <cameron.taggart@gmail.com>
2014-06-19 18:20:49 -0500

    replaced psake with FAKE for building Burrows

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-src\.nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion 
+src\.nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 src\.nuget\nuget.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
-src\.nuget\nuget.exe restore
+src\.nuget\nuget.exe install NUnit.Runners -OutputDirectory packages
+src\.nuget\nuget.exe restore src\Burrows.sln
 packages\FAKE\tools\FAKE.exe build.fsx %*
--- build.fsx
+++ build.fsx
@@ -12,19 +12,17 @@ let dt = DateTime.UtcNow
 let cfg = getBuildConfig __SOURCE_DIRECTORY__
 let repo = new GitRepo(__SOURCE_DIRECTORY__)
 
-let versionAssembly = cfg.AppSettings.["versionAssembly"].Value // change when incompatible
-let versionFile = cfg.AppSettings.["versionFile"].Value // matches nuget version
-let prerelease =
-    if hasBuildParam "prerelease" then getBuildParam "prerelease"
-    else sprintf "ci%s" (dt.ToString "yyMMddHHmm") // 20 char limit
-let buildVersion = if String.IsNullOrEmpty prerelease then versionFile else sprintf "%s-%s" versionFile prerelease
-let versionInfo = sprintf """{"buildVersion":"%s","buildDate":"%s","gitCommit":"%s"}""" buildVersion dt.IsoDateTime repo.Revision // json
+let versionAssembly = getBuildParamOrDefault "versionAssembly" cfg.AppSettings.["versionAssembly"].Value
+let versionFile = getBuildParamOrDefault "versionFile" cfg.AppSettings.["versionFile"].Value
+let prerelease = getBuildParamOrDefault "prerelease" (sprintf "ci%s" (dt.ToString "yyMMddHHmm")) // 20 char limit
+let rawUrl = getBuildParamOrDefault "rawUrl" "https://raw.githubusercontent.com/ctaggart/burrows/{0}/%var2%"
 
-Target "Clean" (fun _ -> 
-    !! "**/bin/"
-    ++ "**/obj/" 
-    |> CleanDirs 
-)
+let buildVersion = if String.IsNullOrEmpty prerelease then versionFile else sprintf "%s-%s" versionFile prerelease
+let versionInfo =
+    let vi = sprintf """{"buildVersion":"%s","buildDate":"%s","gitCommit":"%s"}""" buildVersion dt.IsoDateTime repo.Revision // json
+    vi.Replace("\"","\\\"") // escape quotes
+
+Target "Clean" (fun _ -> !! "**/bin/" ++ "**/obj/" |> CleanDirs)
 
 Target "BuildVersion" (fun _ ->
     let args = sprintf "UpdateBuild -Version \"%s\"" buildVersion
@@ -32,94 +30,124 @@ Target "BuildVersion" (fun _ ->
 )
 
 Target "AssemblyInfo" (fun _ ->
-    // C:\Projects\Burrows\src\Burrows\Properties\AssemblyInfo.cs
-//    [assembly: AssemblyDescription("Burrows is a distributed application framework for .NET forked from MassTransit.")]
-
-    // C:\Projects\Burrows\src\Containers\Burrows.Autofac\Properties\AssemblyInfo.cs
-    // [assembly: AssemblyDescription("Autofac integration for burrows. Burrows is a distributed application framework for .NET forked from MassTransit.")]
-
-    // C:\Projects\Burrows\src\Loggers\Burrows.NLog\Properties\AssemblyInfo.cs
-    // [assembly: AssemblyDescription("NLog integration for burrows. Burrows is a distributed application framework for .NET forked from MassTransit.")]
-
-//    C:\Projects\Burrows\src\Loggers\Burrows.Log4Net\Properties\AssemblyInfo.cs
-//    [assembly: AssemblyDescription("Log4Net integration for burrows. Burrows is a distributed application framework for .NET forked from MassTransit.")]
-
-//    C:\Projects\Burrows\src\Persistence\Burrows.NHib\Properties\AssemblyInfo.cs
-//    [assembly: AssemblyDescription("NHibernate persistence integration for burrows. Burrows is a distributed application framework forked from MassTransit.")]
-
-    CreateFSharpAssemblyInfo "ProtoParser/AssemblyInfo.fs"
-        [ 
-        Attribute.Version versionAssembly 
+    let common = [
+        Attribute.Product "Burrows"
+        Attribute.Copyright "Copyright 2014 Eric Swann, et. al. - All rights reserved."
+        Attribute.Version versionAssembly
         Attribute.FileVersion versionFile
-        Attribute.InformationalVersion (versionInfo.Replace("\"","\\\"")) // escape quotes
-        ]
-    CreateFSharpAssemblyInfo "Roslyn/AssemblyInfo.fs"
-        [ 
-        Attribute.Version versionAssembly 
-        Attribute.FileVersion versionFile
-        Attribute.InformationalVersion (versionInfo.Replace("\"","\\\"")) // escape quotes
-        ]
+        Attribute.InformationalVersion versionInfo
+        Attribute.ComVisible false
+        Attribute.CLSCompliant false ]
+
+    [   Attribute.Description "Burrows is a distributed application framework for .NET forked from MassTransit."
+        Attribute.Guid "1bc31ebf-ea35-4389-8bae-1ced4544e3f6"
+    ] @ common |> CreateCSharpAssemblyInfo @"src\Burrows\Properties\AssemblyInfo.cs"
+
+    [   Attribute.Description "Autofac integration for burrows. Burrows is a distributed application framework for .NET forked from MassTransit."
+    ] @ common |> CreateCSharpAssemblyInfo @"src\Containers\Burrows.Autofac\Properties\AssemblyInfo.cs"
+
+    [   Attribute.Description "NLog integration for burrows. Burrows is a distributed application framework for .NET forked from MassTransit."
+    ] @ common |> CreateCSharpAssemblyInfo @"src\Loggers\Burrows.NLog\Properties\AssemblyInfo.cs"
+
+    [   Attribute.Description "Log4Net integration for burrows. Burrows is a distributed application framework for .NET forked from MassTransit."
+    ] @ common |> CreateCSharpAssemblyInfo @"src\Loggers\Burrows.Log4Net\Properties\AssemblyInfo.cs"
+
+    [   Attribute.Description "NHibernate persistence integration for burrows. Burrows is a distributed application framework forked from MassTransit."
+    ] @ common |> CreateCSharpAssemblyInfo @"src\Persistence\Burrows.NHib\Properties\AssemblyInfo.cs"
 )
 
 Target "Build" (fun _ ->
-    !! "Froto.sln" |> MSBuildRelease "" "Rebuild" |> ignore
+    !! @"src\Burrows.sln" |> MSBuildRelease "" "Rebuild" |> ignore
+)
+
+// NUnit-Console Command Line Options
+// http://www.nunit.org/index.php?p=consoleCommandLine&r=2.6.3
+Target "Test" (fun _ ->
+    let dlls =
+        !! @"tests\Burrows.Tests.dll"
+        ++ @"tests\Burrows.Tests.Framework.dll"
+//        ++ @"tests\Burrows.Tests.RabbitMq.dll" // requires 127.0.0.1:5672
+        ++ @"tests\Burrows.Containers.Tests.dll"
+    dlls |> Seq.iter (logfn "run unit tests: %s")
+    dlls |> NUnit (fun p -> { p with ErrorLevel = Error; Framework="net-4.5" })
 )
 
 Target "SourceLink" (fun _ ->
-    !! "ProtoParser/Froto.Parser.fsproj" 
-    ++ "Roslyn/Froto.Roslyn.fsproj"
+    !! @"src\Burrows\Burrows.csproj"
+    ++ @"src\Containers\Burrows.Autofac\Burrows.Autofac.csproj"
+    ++ @"src\Loggers\Burrows.NLog\Burrows.NLog.csproj"
+    ++ @"src\Loggers\Burrows.Log4Net\Burrows.Log4Net.csproj"
+    ++ @"src\Persistence\Burrows.NHib\Burrows.NHib.csproj"
     |> Seq.iter (fun f ->
         let proj = VsProj.LoadRelease f
         logfn "source linking %s" proj.OutputFilePdb
-        let files = proj.Compiles -- "**/AssemblyInfo.fs"
+        let files = proj.Compiles -- "**/AssemblyInfo.cs"
         repo.VerifyChecksums files
         proj.VerifyPdbChecksums files
-        proj.CreateSrcSrv "https://raw.github.com/ctaggart/froto/{0}/%var2%" repo.Revision (repo.Paths files)
+        proj.CreateSrcSrv rawUrl repo.Revision (repo.Paths files)
         Pdbstr.exec proj.OutputFilePdb proj.OutputFilePdbSrcSrv
     )
 )
 
 Target "NuGet" (fun _ ->
-    let bin = "bin"
-    Directory.CreateDirectory bin |> ignore
+    CreateDir "bin"
+    let common (p:NuGetParams) =
+        { p with
+            Version = buildVersion
+            OutputPath = "bin"
+        }
 
-    NuGet (fun p -> 
-    { p with
-        Version = buildVersion
-        WorkingDir = "ProtoParser/bin/Release"
-        OutputPath = bin
-        DependenciesByFramework =
-        [{ 
-            FrameworkVersion = "net45"
-            Dependencies = 
-                [
-                "FParsec", GetPackageVersion "./packages/" "FParsec"
-                ] 
-        }]
-    }) "ProtoParser/Froto.Parser.nuspec"
+    NuGet (fun p ->
+        { p with
+            WorkingDir = @"src\Burrows\bin\Release"
+            Dependencies =
+                [   "RabbitMQ.Client", GetPackageVersion "src/packages" "RabbitMQ.Client"
+                    "Stact", GetPackageVersion "src/packages" "Stact"
+                    "Magnum", GetPackageVersion "src/packages" "Magnum"
+                    "Newtonsoft.Json", GetPackageVersion "src/packages" "Newtonsoft.Json" ]
+        } |> common) @"src\Burrows\Burrows.nuspec"
 
-    NuGet (fun p -> 
-    { p with
-        Version = buildVersion
-        WorkingDir = "Roslyn/bin/Release"
-        OutputPath = bin
-        DependenciesByFramework =
-        [{ 
-            FrameworkVersion = "net45"
-            Dependencies = 
-                [
-                "Froto.Parser", sprintf "[%s]" buildVersion // exact version
-                "Microsoft.CodeAnalysis.CSharp.Workspaces", GetPackageVersion "./packages/" "Microsoft.CodeAnalysis.CSharp.Workspaces"
-                ] 
-        }]
-    }) "Roslyn/Froto.Roslyn.nuspec"
+    NuGet (fun p ->
+        { p with
+            WorkingDir = @"src\Containers\Burrows.Autofac\bin\Release"
+            Dependencies =
+                [   "Burrows", buildVersion
+                    "Autofac", GetPackageVersion "src/packages" "Autofac"]
+        } |> common) @"src\Containers\Burrows.Autofac\Burrows.Autofac.nuspec"
+
+    NuGet (fun p ->
+        { p with
+            WorkingDir = @"src\Loggers\Burrows.NLog\bin\Release"
+            Dependencies =
+                [   "Burrows", buildVersion
+                    "NLog", GetPackageVersion "src/packages" "NLog"]
+        } |> common) @"src\Loggers\Burrows.NLog\Burrows.NLog.nuspec"
+
+    NuGet (fun p ->
+        { p with
+            WorkingDir = @"src\Loggers\Burrows.Log4Net\bin\Release"
+            Dependencies =
+                [   "Burrows", buildVersion
+                    "log4net", GetPackageVersion "src/packages" "log4net"]
+        } |> common) @"src\Loggers\Burrows.Log4Net\Burrows.Log4Net.nuspec"
+
+    NuGet (fun p ->
+        { p with
+            WorkingDir = @"src\Persistence\Burrows.NHib\bin\Release"
+            Dependencies =
+                [   "Burrows", buildVersion
+                    "NHibernate", GetPackageVersion "src/packages" "NHibernate"]
+        } |> common) @"src\Persistence\Burrows.NHib\Burrows.NHib.nuspec"
 )
 
-"Clean"
+Target "Start" DoNothing
+
+"Start"
     =?> ("BuildVersion", buildServer = BuildServer.AppVeyor)
     ==> "AssemblyInfo"
     ==> "Build"
-    =?> ("SourceLink", buildServer = BuildServer.AppVeyor || hasBuildParam "sl")
+    ==> "Test"
+    =?> ("SourceLink", buildServer = BuildServer.AppVeyor || hasBuildParam "link")
     ==> "NuGet"
 
 RunTargetOrDefault "NuGet"
